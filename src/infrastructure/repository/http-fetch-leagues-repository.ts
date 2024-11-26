c310f914ddaf9a4cc68149eb66a3e1b1b7a660b2
Ian Pedro <ianviana_2009@hotmail.com>
2024-11-26 00:18:07 -0300

    fix: fixing baseUrl trouble

--- src/infrastructure/repository/http-fetch-leagues-repository.ts
+++ src/infrastructure/repository/http-fetch-leagues-repository.ts
@@ -34,6 +34,7 @@ export class HttpFetchLeaguesRepository implements FetchLeaguesRepository {
       headers: {
         "x-rapidapi-host": "v3.football.api-sports.io",
         "x-rapidapi-key": String(process.env.API_KEY),
+        "Content-Type": "application/json",
       },
     });
 
