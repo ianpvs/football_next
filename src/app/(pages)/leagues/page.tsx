aeaf6e470e16b22f57b8e3db08ec9abb9d1fb03d
Ian Pedro <ianviana_2009@hotmail.com>
2024-11-25 23:51:33 -0300

    fix: fixing page fetch url

--- src/app/(pages)/leagues/page.tsx
+++ src/app/(pages)/leagues/page.tsx
@@ -2,15 +2,12 @@ import ListLeagues from "./components/list-leagues";
 import { getRevalidateTimeInMinutes } from "@/utils/revalidate";
 
 export default async function Leagues() {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/leagues`,
-    {
-      method: "GET",
-      next: {
-        revalidate: getRevalidateTimeInMinutes(60),
-      },
-    }
-  );
+  const response = await fetch(`/api/leagues`, {
+    method: "GET",
+    next: {
+      revalidate: getRevalidateTimeInMinutes(60),
+    },
+  });
 
   const leagues = await response.json();
 
